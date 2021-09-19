5c63375131655069ab2889bb3c03e473bc09a2b4
FethiOuail <anwarinfo26@gmail.com>
2021-09-19 23:04:28 +0100

    ffffffff

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,11 +4,11 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Home</title>	
+	<title>Home</title>
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/favicon.ico')}}">
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic,900,900italic&amp;subset=latin,latin-ext" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Open%20Sans:300,400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext" rel="stylesheet">
-	
+
 	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/animate.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-awesome.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css')}}">
@@ -53,7 +53,7 @@
 										<li class="menu-item" ><a title="canada" href="#"><span class="img label-before"><img src="{{asset('assets/images/lang-can.png')}}" alt="lang-can"></span>Canada</a></li>
 									</ul>
 								</li>
-								
+
 								@if (Route::has('login'))
 
 									@auth
@@ -72,15 +72,15 @@
 
 													<form id="logout-form" method="POST" action=" {{ route('logout') }} ">
 														@csrf
-													
+
 												    </form>
 
 												</ul>
 											</li>
 
-										@else 
+										@else
 
-										
+
 											<li class="menu-item menu-item-has-children parent" >
 												<a title="My Account" href="#"> My Account ({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
 												<ul class="submenu curency" >
@@ -94,21 +94,21 @@
 
 													<form id="logout-form" method="POST" action=" {{ route('logout') }} ">
 														@csrf
-													
+
 												    </form>
-													
+
 												</ul>
 											</li>
-											
+
 										@endif
-										
+
 									@else
 										<li class="menu-item" ><a title="Register or Login" href="{{ route('login') }}">Login</a></li>
 										<li class="menu-item" ><a title="Register or Login" href="{{ route('register')}}">Register</a></li>
-									
+
 									@endif
 
-									
+
 								@endif
 							</ul>
 						</div>
@@ -169,13 +169,13 @@
 									<i class="fa fa-shopping-basket" aria-hidden="true"></i>
 									<div class="left-info">
 										@if (Cart::count() > 0)
-										
+
 										<span class="index"> {{ Cart::count() }} items</span>
 
 										@endif
-										
+
 										<span class="title">CART</span>
-								
+
 									</div>
 								</a>
 							</div>
@@ -224,7 +224,7 @@
 								</li>
 								<li class="menu-item">
 									<a href="contact-us.html" class="link-term mercado-item-title">Contact Us</a>
-								</li>																	
+								</li>
 							</ul>
 						</div>
 					</div>
@@ -302,7 +302,7 @@
 											<li>
 												<i class="fa fa-envelope" aria-hidden="true"></i>
 												<p class="contact-txt">Contact@yourcompany.com</p>
-											</li>											
+											</li>
 										</ul>
 									</div>
 								</div>
@@ -477,10 +477,10 @@
 					<div class="coppy-right-item item-right">
 						<div class="wrap-nav horizontal-nav">
 							<ul>
-								<li class="menu-item"><a href="about-us.html" class="link-term">About us</a></li>								
+								<li class="menu-item"><a href="about-us.html" class="link-term">About us</a></li>
 								<li class="menu-item"><a href="privacy-policy.html" class="link-term">Privacy Policy</a></li>
 								<li class="menu-item"><a href="terms-conditions.html" class="link-term">Terms & Conditions</a></li>
-								<li class="menu-item"><a href="return-policy.html" class="link-term">Return Policy</a></li>								
+								<li class="menu-item"><a href="return-policy.html" class="link-term">Return Policy</a></li>
 							</ul>
 						</div>
 					</div>
@@ -489,12 +489,12 @@
 			</div>
 		</div>
 	</footer>
-	
+
 	<script src="{{ asset('assets/js/jquery-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
 	<script src="{{ asset('assets/js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
 	<script src="{{ asset('assets/js/bootstrap.min.js')}}"></script>
 	<script src="{{ asset('assets/js/jquery.flexslider.js')}}"></script>
-	<script src="{{ asset('assets/js/chosen.jquery.min.js')}}"></script>
+{{--	<script src="{{ asset('assets/js/chosen.jquery.min.js')}}"></script>--}}
 	<script src="{{ asset('assets/js/owl.carousel.min.js')}}"></script>
 	<script src="{{ asset('assets/js/jquery.countdown.min.js')}}"></script>
 	<script src="{{ asset('assets/js/jquery.sticky.js')}}"></script>
