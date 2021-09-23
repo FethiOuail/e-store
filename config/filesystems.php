661352b05abfd04e37fcd0fd3cfda373e6733438
FethiOuail <anwarinfo26@gmail.com>
2021-09-23 13:11:27 +0100

    ffff

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,9 +38,9 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-           // 'root' => public_path('assets/images'),
-            'url' => env('APP_URL').'/storage',
+           // 'root' => storage_path('app/public'),
+            'root' => public_path('assets/images'),
+            'url' => env('APP_URL').'/public',
             'visibility' => 'public',
         ],
 
